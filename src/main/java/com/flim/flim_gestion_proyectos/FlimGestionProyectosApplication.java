0d6b33b93b9f598e54fc33ddb50e0f936bb20962
Lendrock <lchoxon-2021286@kinal.edu.gt>
2025-09-02 07:51:54 -0600

    Creacion del proyecto flim_Gestion-Proyectos

---
+++ src/main/java/com/flim/flim_gestion_proyectos/FlimGestionProyectosApplication.java
@@ -0,0 +1,13 @@
+package com.flim.flim_gestion_proyectos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlimGestionProyectosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlimGestionProyectosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flim/flim_gestion_proyectos/FlimGestionProyectosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flim.flim_gestion_proyectos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlimGestionProyectosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
