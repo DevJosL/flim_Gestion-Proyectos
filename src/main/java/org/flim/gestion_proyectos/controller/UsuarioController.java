8ec005e13418b4c6da8b37b8605aef123bf1384e
DevJosL <jmoran-2022388@kinal.edu.gt>
2025-09-05 22:44:54 -0600

    Se realizo el controller de usuarios

---
+++ src/main/java/org/flim/gestion_proyectos/controller/UsuarioController.java
@@ -0,0 +1,59 @@
+package org.flim.gestion_proyectos.controller;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.faces.view.ViewScoped;
+import lombok.Data;
+import org.flim.gestion_proyectos.entity.Usuario;
+import org.flim.gestion_proyectos.service.IUsuarioService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@ViewScoped
+@Data
+public class UsuarioController {
+
+    @Autowired
+    IUsuarioService usuarioService;
+    private List<Usuario> usuarios;
+    private Usuario usuarioSeleccionado;
+    private static Logger logger = LoggerFactory.getLogger(UsuarioController.class);
+
+    @PostConstruct
+    public void init(){
+        cargarDatos();
+    }
+
+    public void cargarDatos(){
+        this.usuarios = this.usuarioService.listarUsuario();
+        this.usuarios.forEach(usuario -> logger.info(usuario.toString()));
+    }
+
+    public void agregarUsuario(){
+        this.usuarioSeleccionado = new Usuario();
+    }
+
+    public void guardarUsuario(){
+        logger.info("Usuario a guardar" + this.usuarioSeleccionado);
+
+        if (this.usuarioSeleccionado.getIdUsuario()==null){
+            this.usuarioService.guardarUsuario(this.usuarioSeleccionado);
+            this.usuarios.add(this.usuarioSeleccionado);
+        } else{
+            this.usuarioService.guardarUsuario(this.usuarioSeleccionado);
+        }
+
+        this.usuarioSeleccionado = null;
+    }
+
+    public void eliminarUsuari(){
+        logger.info("Usuario a eliminar: " + this.usuarioSeleccionado);
+        this.usuarioService.eliminarUsuario(this.usuarioSeleccionado);
+        this.usuarios.remove(this.usuarioSeleccionado);
+        this.usuarioSeleccionado = null;
+    }
+}
